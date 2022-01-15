84064cce31370d6604695881e4282195b2f98c0d
Taylor Pool <taylorpool.27@gmail.com>
2022-01-14 18:15:27 -0700

    Refactored for cleaner indexing

--- src/Rotations.jl
+++ src/Rotations.jl
@@ -3,37 +3,17 @@ module Rotations
 export Quaternion
 
 mutable struct Quaternion{T} <: AbstractVector{T}
-    a::T
-    b::T
-    c::T
-    d::T
+    data::Vector{T}
 end
 
 Base.size(q::Quaternion) = (4,)
 
 Base.getindex(q::Quaternion, i::Int) = begin
-    if i == 1
-        value = q.a
-    elseif i == 2
-        value = q.b
-    elseif i == 3
-        value = q.c
-    elseif i == 4
-        value = q.d
-    end
-    value
+    q.data[i]
 end
 
 Base.setindex!(q::Quaternion, value, i::Int) = begin
-    if i == 1
-        q.a = value
-    elseif i == 2
-        q.b = value
-    elseif i == 3
-        q.c = value
-    elseif i == 4
-        q.d = value
-    end
+    q.data[i] = value
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 using Rotations
 
 x = [1.0; 0.0; 0.0; 0.0]
-quat = Quaternion(x...)
+quat = Quaternion(x)
 
 @test quat[1] == 1.0
 @test quat[2] == 0.0
@@ -13,8 +13,8 @@ quat = Quaternion(x...)
 quat[2] = 1.0
 @test quat[2] == 1.0
 
-q1 = Quaternion(x...)
-twoq1 = Quaternion(2*x...)
+q1 = Quaternion(x)
+twoq1 = Quaternion(2*x)
 
 @test (q1+q1) == twoq1
 
