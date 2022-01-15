57cbcf62c64fb9c7a926aa3a594aa4534fe65c4e
Taylor Pool <taylorpool.27@gmail.com>
2022-01-14 18:41:53 -0700

    Added quaternion multiplication (Hamiltonian product)

--- src/Rotations.jl
+++ src/Rotations.jl
@@ -16,4 +16,13 @@ Base.setindex!(q::Quaternion, value, i::Int) = begin
     q.data[i] = value
 end
 
+Base.:*(q1::Quaternion, q2::Quaternion) = begin
+    Quaternion([
+        q1[1]*q2[1] - q1[2]*q2[2] - q1[3]*q2[3] - q1[4]*q2[4]
+        q1[1]*q2[2] + q1[2]*q2[1] + q1[3]*q2[4] - q1[4]*q2[3]
+        q1[1]*q2[3] - q1[2]*q2[4] + q1[3]*q2[1] + q1[4]*q2[2]
+        q1[1]*q2[4] + q1[2]*q2[3] - q1[3]*q2[2] + q1[4]*q2[1]
+    ])
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -19,3 +19,25 @@ twoq1 = Quaternion(2*x)
 @test (q1+q1) == twoq1
 
 @test 2*q1 == twoq1
+
+a1 = 1.0
+b1 = 2.0
+c1 = 3.0
+d1 = 4.0
+a2 = -1.0
+b2 = -2.0
+c2 = -3.0
+d2 = -4.0
+
+a = [a1; b1; c1; d1]
+b = [a2; b2; c2; d2]
+
+q1 = Quaternion(a)
+q2 = Quaternion(b)
+
+multiplyq = q1*q2
+
+@test multiplyq[1] == a[1]*b[1] - sum(a[2:end].*b[2:end])
+@test multiplyq[2] == (a1*b2 + b1*a2 + c1*d2 - d1*c2)
+@test multiplyq[3] == (a1*c2 - b1*d2 + c1*a2 + d1*b2)
+@test multiplyq[4] == (a1*d2 + b1*c2 - c1*b2 + d1*a2)
